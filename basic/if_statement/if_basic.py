beb07366b840f3350d0c53f8e490a9519fef40ed
Shvana <htsunemikuv9@gmail.com>
2021-03-30 20:04:59 -0600

    "if" statement

---
+++ basic/if_statement/if_basic.py
@@ -0,0 +1,9 @@
+age = int(input('Enter age: '))
+if age < 13:
+    print('You are a preteen.')
+elif age < 20:
+    print('You are a teenager')
+elif age <= 30:
+    print('You are still young.')
+else:
+    print('You are one of the oldies')
