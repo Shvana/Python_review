4949c693c554c462ca0cbe95d97e7cad4bedec00
Shvana <htsunemikuv9@gmail.com>
2021-03-30 19:45:20 -0600

    function definition example

---
+++ basic/funtions_definitions/functions.py
@@ -0,0 +1,6 @@
+def main():
+    side1 = float(input('Enter length of a side: '))
+    side2 = float(input('Enter another length: '))
+
+    hyp = ((side1 * side2) + (side2 * side2)) ** 0.5
+    print('Length of hypotenuse is:', hyp)
